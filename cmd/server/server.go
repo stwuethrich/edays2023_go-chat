b64a25099729cd907cfc094de97de8b26d94a632
Stefan <stwuethrich@swissonline.ch>
2023-08-24 13:27:07 +0200

    echo server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
 func main() {
+	// listen on port 8000
+	ln, _ := net.Listen("tcp", ":8000")
+
+	// accept connection
+	conn, _ := ln.Accept()
+
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+
 	println("Hello Server!")
+
+	fmt.Printf("% X\n", message)
+	conn.Write([]byte(message))
 }
